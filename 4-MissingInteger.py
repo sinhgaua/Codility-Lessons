65f5428dc8b5427e8a2e6e959faed82a47dfd9ec
OEM Configuration (temporary user) <gauravsinha.15@gmail.com>
2019-03-20 18:25:08 +0000

    Updating 4-MissingInteger

--- 4-MissingInteger.py
+++ 4-MissingInteger.py
@@ -1,26 +1,34 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Tue Mar 19 23:01:39 2019
+This is a demo task.
 
-@author: oem
-"""
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
-For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
 Write a function:
-def solution(N)
-that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
-For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+
+def solution(A)
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
 Write an efficient algorithm for the following assumptions:
-N is an integer within the range [1..2,147,483,647].
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
 """
 
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
-def solution(N):
-    return max([len(x) for x in "{0:b}".format(N).rstrip('0').lstrip('0').split('1')])
+
+def solution(A):
+    counter = [False]*len(A)
+    for item in A:
+        if 0<item<=len(A):
+            counter[item-1]= True
+    for idx in range(len(counter)):
+        if counter[idx]==False:
+            return idx+1
+    return len(A) +1
